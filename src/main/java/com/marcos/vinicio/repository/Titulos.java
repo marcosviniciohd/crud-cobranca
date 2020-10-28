2d746137eb18f29304c31801ac25433d1f976878
Marcos Fernandes <marcosviniciohd@hotmail.com>
2020-10-28 07:10:15 -0300

    Implementação da classe Titulo, TituloController, repositório Titulos e da tela CadastroTitulo.html

---
+++ src/main/java/com/marcos/vinicio/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.marcos.vinicio.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marcos.vinicio.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long>{
+
+}
---
+++ src/test/java/com/marcos/vinicio/CobrancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.vinicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobrancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
